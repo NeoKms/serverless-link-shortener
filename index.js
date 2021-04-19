3e4fa1b5156483798135b966fa31b89ddc3cbb0d
neokms <upachko@gmail.com>
2021-04-20 00:37:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ exports.handler = async function (event, context) {
     }
     return await getResult(url, event, context)
 };
-async function getResult(url, event, context) {
+function getResult(url, event, context) {
     console.log(url);
     if (url == "/shortennode") {
-        return await shorten(event,false)
+        return shorten(event,false)
     }
     if (url == "/shortennode2") {
-        return await shorten(event,true)
+        return shorten(event,true)
     }
     if (url.indexOf("/rnode/")!==-1) {
-        return await redirect(event,context)
+        return redirect(event,context)
     }
 
     return return404('несуществующий путь');
 }
-async function shorten(event, fullNode) {
+function shorten(event, fullNode) {
     body = event.body
     if (body !== undefined && body!=='') {
         let original_host = event.headers.Origin;
@@ -36,8 +36,8 @@ async function shorten(event, fullNode) {
         console.log('original_host',original_host);
         console.log('shorturl',shorturl)
         let unquoteBody = unquote(body);
-        let res = await insertLink(link_id,unquoteBody);
-        return {
+        return insertLink(link_id,unquoteBody)
+          .then(noRes=>{
             'statusCode': 200,
             'headers': {
                 'Content-Type': 'application/json'
@@ -47,25 +47,36 @@ async function shorten(event, fullNode) {
                 'url': shorturl,
                 'result_insert': res
             })
-        }
+        })
+        .catch(err=>{
+            console.log(err);
+            return return404('Внутренняя ошибка')
+        })
     }
     return return404('не передан урл')
 }
-async function redirect(event,context) {
+function redirect(event,context) {
     console.log('redir');
     console.log('params',event.params);
-    let result = await getLink(event.params.id);
-    if (result!==false) {
-        return {
-            'statusCode': 302,
-            'headers': {
-                'Location': result
-            },
-            'isBase64Encoded': false,
-            'body': ''
+    let result = await getLink(event.params.id)
+    .then( result => {
+        if (result!==false) {
+            return {
+                'statusCode': 302,
+                'headers': {
+                    'Location': result
+                },
+                'isBase64Encoded': false,
+                'body': ''
+            }
+        } else {
+            throw new Error('no link')
         }
-    }
-    return return404('Данной ссылки не существует')
+    })
+      .catch(err=>{
+           console.log(err)
+           return return404('Данной ссылки не существует')
+      })
 }
 function return404(text) {
     return {
@@ -88,7 +99,7 @@ function sha256(message) {
     return hash.substr(0,6)
 }
 
-async function insertLink(id,url) {
+function insertLink(id,url) {
     //инитим базу
     const logger = getLogger({level: 'debug'});
     const entryPoint = 'grpcs://'+process.env.database;
@@ -96,20 +107,28 @@ async function insertLink(id,url) {
     const authService = getCredentialsFromEnv(entryPoint, dbName, logger);
     const driver = new Driver(entryPoint, dbName, authService);
     //
-    if (!await driver.ready(10000)) {
-        logger.fatal(`Driver has not become ready in 10 seconds!`);
-        process.exit(1);
-    }
-    return await driver.tableClient.withSession(async (session) => {
-        if (await getLink(id) === false) {
-            let query = "PRAGMA TablePathPrefix(\""+dbName+"\");INSERT INTO links (id,link) VALUES ('"+id+"','"+url+"')";
-            console.log('insert query',query)
-            const {resultSets} = await session.executeQuery(query);
-            return resultSets;
-        } else {
-            return 'link exist in db'
+    return driver.ready(10000)
+    .then(res=>{
+        if (!res) {
+            throw new Error(`Driver has not become ready in 10 seconds!`)
         }
-    });
+    })
+    .then(noRes=>{
+        return driver.tableClient.withSession(async (session) => {
+            if (await getLink(id) === false) {
+                let query = "PRAGMA TablePathPrefix(\""+dbName+"\");INSERT INTO links (id,link) VALUES ('"+id+"','"+url+"')";
+                console.log('insert query',query)
+                const {resultSets} = await session.executeQuery(query);
+                return resultSets;
+            } else {
+                return 'link exist in db'
+            }
+        });
+    })
+    .catch(err=>{
+        logger.fatal(err.getMessage());
+        process.exit(1);
+    })
 }
 async function getLink(id) {
     //инитим базу
@@ -119,20 +138,28 @@ async function getLink(id) {
     const authService = getCredentialsFromEnv(entryPoint, dbName, logger);
     const driver = new Driver(entryPoint, dbName, authService);
     //
-    if (!await driver.ready(10000)) {
-        logger.fatal(`Driver has not become ready in 10 seconds!`);
-        process.exit(1);
-    }
-    return await driver.tableClient.withSession(async (session) => {
-        let query = "PRAGMA TablePathPrefix(\""+dbName+"\");SELECT link FROM links WHERE id=\""+id+"\"";
-        console.log('select query',query);
-        return await session.executeQuery(query).then(res => {b = res}).then(() => {
-            if (b.resultSets[0].rows[0] == undefined) {
-                console.log('link not exist');
-                return false;
-            }
-            console.log('link is ',b.resultSets[0].rows[0].items[0].textValue);
-            return b.resultSets[0].rows[0].items[0].textValue
+    return driver.ready(10000)
+    .then(res=>{
+        if (!res) {
+            throw new Error(`Driver has not become ready in 10 seconds!`)
+        }
+    })
+    .then(noRes=>{
+        return driver.tableClient.withSession(async (session) => {
+            let query = "PRAGMA TablePathPrefix(\""+dbName+"\");SELECT link FROM links WHERE id=\""+id+"\"";
+            console.log('select query',query);
+            return await session.executeQuery(query).then(res => {b = res}).then(() => {
+                if (b.resultSets[0].rows[0] == undefined) {
+                    console.log('link not exist');
+                    return false;
+                }
+                console.log('link is ',b.resultSets[0].rows[0].items[0].textValue);
+                return b.resultSets[0].rows[0].items[0].textValue
+            });
         });
-    });
+    })
+    .catch(err=>{
+        logger.fatal(err.getMessage());
+        process.exit(1);
+    })
 }
